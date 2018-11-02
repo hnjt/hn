ed40c1dc8d7347f756cb2ab9186042459dc4f371
hnjt <18645636597@163.com>
2018-11-02 11:41:38 +0800

    pom core_server

---
+++ hn_pojo/src/main/java/com/hn/hn_pojo/HnCoreServerApplication.java
@@ -0,0 +1,12 @@
+package com.hn.hn_pojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnCoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.hn.hn_core_server.HnCoreServerApplication.class, args);
+    }
+}
