9bff81fbbcd43b59331c01909226ac37626f8b76
githubchenyb <github18645636597>
2018-11-03 21:15:05 +0800

    polymerization framework init

--- hn_core_server/src/main/java/com/hn/hn_core_server/HnCoreServerApplication.java
+++ hn_core_server/src/main/java/com/hn/hn_core_server/HnCoreServerApplication.java
@@ -3,7 +3,9 @@ package com.hn.hn_core_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HnCoreServerApplication extends SpringBootServletInitializer {
 
