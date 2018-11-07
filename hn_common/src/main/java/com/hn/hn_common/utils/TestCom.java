5cf24f4fad9e3bece196cdc4b6d89359a203ec55
hnjt <18645636597@163.com>
2018-11-07 18:17:57 +0800

    add mai test

--- hn_common/src/main/java/com/hn/hn_common/utils/TestCom.java
+++ hn_common/src/main/java/com/hn/hn_common/utils/TestCom.java
@@ -5,8 +5,13 @@ package com.hn.hn_common.utils;
  */
 public class TestCom {
 
-    public String testGet (String str){
+    public static String testGet (String str){
 
         return str + "Common Test Ok";
+    }
+
+    public static void main(String[] args) {
+        String s = TestCom.testGet("Chenyb : ");
+        System.out.println(s);
     }
 }
