1696704fb0ef31d2f4de57db06a096f0c3c51123
hnjt <18645636597@163.com>
2018-11-19 16:39:05 +0800

    Eureka 0.0.2 Test SUCCESS

--- hn_core_web/src/main/java/com/hn/hn_core_web/web/UserLoginController.java
+++ hn_core_web/src/main/java/com/hn/hn_core_web/web/UserLoginController.java
@@ -5,12 +5,13 @@ import com.hn.hn_pojo.Entity.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class UserLoginController {
 
     @RequestMapping("test")
@@ -24,12 +25,12 @@ public class UserLoginController {
     @Value("${hn_core_server}")
     private String hn_core_server;
 
-    @GetMapping("/test")
+    /*@GetMapping("/test")
     public String testGetList2 (Class responseType){
 
         ResponseEntity<String> forEntity = this.restTemplate.getForEntity(hn_core_server + "/test/testGetList", String.class);
         return forEntity.getBody();
-    }
+    }*/
 
     @GetMapping("/test2")
     public List sss (Class responseType){
@@ -42,6 +43,7 @@ public class UserLoginController {
     private UserLoginService userLoginImpl;
 
     @GetMapping("/test3")
+    @ResponseBody
     public List<UserVO> ccc (String uuid){
 
         System.out.println(uuid);
